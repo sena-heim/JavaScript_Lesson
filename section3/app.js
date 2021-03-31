aebc2451ea0a2891ab4c282e3105ed9cbe461792
sen-com <nogenon@docomo.ne.jp>
2021-04-01 03:55:19 +0900

    finish s3

--- section3/app.js
+++ section3/app.js
@@ -1,22 +1,96 @@
-// $(document).ready(function () {
-//   $('body').html('<h1>Hello jQuery!!</h1>');
-// });
+// // メイン部分
+// var alertString;
+// alertString = addString("WebCamp");
 
-// $(function(){
-//   $('.box1').css({
-//     'background-color': '#0000FF',
-//     'height': '100px'
-//   });
-// });
+// // 作成した関数を呼び出す
+// alert(alertString);
 
-$(function(){
-  // $('.box1').slideDown(2000);
-  // $('.box1').slideUp(2000);
-  // $('.box1').show(2000);
-  // $('.box1').css({'background-color': '#0000FF'});
-  // $('.box1').hide(2000);
-  $('.box1').slideDown(2000);
-  $('.box1').css({"height": "200px",
-                  "width": "100px",
-                  'background-color': '#0000FF'});
-});
+// // 作成した関数
+// function addString(strA){
+//   var addStr = "Hello " + strA;
+//   return addStr;
+// }
+
+
+// var promptStr = prompt('何か好きな文字を入力してください。');
+
+// alert(promptStr);
+
+
+// var promptStr = prompt('何か好きな文字を入力してください。');
+
+// alert(promptStr);
+
+
+// function getJShand(){
+//   var js_hand_num = Math.floor( Math.random() * 3 );
+
+//   if(js_hand_num == 0){
+//     js_hand = "グー";
+//   } else if(js_hand_num == 1){
+//     js_hand = "チョキ";
+//   } else if(js_hand_num == 2){
+//     js_hand = "パー";
+//   }
+
+//   return js_hand;
+// }
+
+
+
+var user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+while ((user_hand != "グー") && (user_hand != "チョキ") && (user_hand != "パー") && (user_hand != null)){
+  alert('グー・チョキ・パーのいずれかを入力して下さい');
+  user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください。');
+}
+var js_hand = getJShand();
+var judge = winLose(user_hand, js_hand);
+
+if (user_hand != null){
+  alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
+} else {
+  alert("またチャレンジしてね")
+}
+
+function getJShand(){
+  var js_hand_num = Math.floor(Math.random() * 3);
+  var hand;
+  if (js_hand_num == 0){
+    hand = "グー";
+  } else if (js_hand_num == 1){
+    hand = "チョキ";
+  } else if (js_hand_num == 2){
+    hand = "パー";
+  }
+  return hand;
+}
+
+function winLose(user, js){
+  var winLoseStr;
+  if (user == "グー"){
+    if (js == "グー"){
+      winLoseStr = "あいこ";
+    } else if (js == "チョキ"){
+      winLoseStr = "勝ち";
+    } else if (js == "パー"){
+      winLoseStr = "負け";
+    }
+  } else if (user == "チョキ"){
+    if (js == "グー"){
+      winLoseStr = "負け";
+    } else if (js == "チョキ"){
+      winLoseStr = "あいこ";
+    } else if (js == "パー"){
+      winLoseStr = "勝ち";
+    }
+  } else if (user == "パー"){
+    if (js == "グー"){
+      winLoseStr = "勝ち";
+    } else if (js == "チョキ"){
+      winLoseStr = "負け";
+    } else if (js == "パー"){
+      winLoseStr = "あいこ";
+    }
+  }
+  return winLoseStr;
+}
