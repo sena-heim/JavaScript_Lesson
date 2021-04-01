43d39dba6bb76271b0801f976f8c89ceed53be45
sen-com <nogenon@docomo.ne.jp>
2021-04-01 10:17:07 +0900

    finish s1

---
+++ section3/app.js
@@ -0,0 +1,22 @@
+// $(document).ready(function () {
+//   $('body').html('<h1>Hello jQuery!!</h1>');
+// });
+
+// $(function(){
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px'
+//   });
+// });
+
+$(function(){
+  // $('.box1').slideDown(2000);
+  // $('.box1').slideUp(2000);
+  // $('.box1').show(2000);
+  // $('.box1').css({'background-color': '#0000FF'});
+  // $('.box1').hide(2000);
+  $('.box1').slideDown(2000);
+  $('.box1').css({"height": "200px",
+                  "width": "100px",
+                  'background-color': '#0000FF'});
+});
