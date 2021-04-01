a93cd1e5d041cb638b68261bb1cbcd6ede4f04eb
sen-com <nogenon@docomo.ne.jp>
2021-04-01 12:15:49 +0900

    finish s2

--- section2/app.js
+++ section2/app.js
@@ -1,22 +1,50 @@
-// $(document).ready(function () {
-//   $('body').html('<h1>Hello jQuery!!</h1>');
-// });
+// alert('Hello' + 'World');
 
-// $(function(){
-//   $('.box1').css({
-//     'background-color': '#0000FF',
-//     'height': '100px'
-//   });
-// });
+// var str1 = 'Hello';
+// var str2 = 'World!!';
+// alert(str1 + str2);
 
-$(function(){
-  // $('.box1').slideDown(2000);
-  // $('.box1').slideUp(2000);
-  // $('.box1').show(2000);
-  // $('.box1').css({'background-color': '#0000FF'});
-  // $('.box1').hide(2000);
-  $('.box1').slideDown(2000);
-  $('.box1').css({"height": "200px",
-                  "width": "100px",
-                  'background-color': '#0000FF'});
-});
+// var orange = 100;
+// var apple = 120;
+
+// if(orange < apple){
+//   alert('みかんの値段がりんごより安い');
+// } else if(orange == apple){
+//   alert('みかんとりんごが同じ値段');
+// } else{
+//   alert('みかんの値段がりんごより高い');
+// }
+
+// var max = 100;
+// var num = 1;
+// var count = 0;
+
+// while(num < max){
+//   num = num * 2;
+//   count = count + 1;
+// }
+
+// alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+// var i;
+// var num = 0;
+
+// for(i = 1; i < 11; i++){
+//   num = num + i;
+// }
+
+// alert('1から10まで足し算した結果は' + num + 'です');
+
+
+
+// 足し算
+// alert(4 + 3);
+
+// 引き算
+// alert(8 - 5);
+
+// 掛け算
+// alert(2 * 6);
+
+// 割り算
+alert(10 / 2);
